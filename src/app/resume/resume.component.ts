1eb2975f783230f5bcf24517a2dfba604c10a62d
Bhagpreet <bb86029@gmail.com>
2021-07-08 15:47:36 -0700

    Fixing mobile screen margins and resume

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { PDFDocumentProxy } from 'ng2-pdf-viewer';
 
 @Component({
   selector: 'app-resume',
-  templateUrl: './resume.component.html',
+  template: '<pdf-viewer [src]="src" [original-size]="false" [autoresize]="true" style="display: block;"></pdf-viewer>',
   styleUrls: ['./resume.component.css']
 })
 export class ResumeComponent implements OnInit {
+  src : string = "https://bhagpreet.github.io/Bhagpreet/assets/pdf/Resume.pdf";
 
+  // @Output('after-load-complete') afterLoadComplete = new EventEmitter<PDFDocumentProxy>();
+  
   constructor() { }
 
   ngOnInit(): void {
